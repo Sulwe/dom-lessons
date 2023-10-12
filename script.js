88ffe1f616607608e4e5e139342fc5c19d9a183f
Sulwe <cypriansulwe.cs@gmail.com>
2023-10-12 21:13:23 +0300

    add event handlers

--- script.js
+++ script.js
@@ -35,3 +35,39 @@ meToo.textContent = "ME TOO!";
 pink.appendChild(meToo);
 
 container.appendChild(pink);
+
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World");
+
+const bttn = document.querySelector('#bttn');
+bttn.addEventListener('click', () => {alert("Hello World");
+});
+function alertFunction(){
+    alert("Yay! You did it!");
+}
+btn.addEventListener('click', alertFunction);
+
+btn.addEventListener('click', function (e){
+    console.log(e);
+});
+
+btn.addEventListener('click', function(e) {
+    console.log(e.target);
+});
+
+btn.addEventListener('click', function(e) {
+    e.target.style.background = 'blue';
+});
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
+});
+
